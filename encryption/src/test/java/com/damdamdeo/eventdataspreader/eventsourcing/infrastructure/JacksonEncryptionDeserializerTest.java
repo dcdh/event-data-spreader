f05d5616bb68f25f6f0b365bb3b0e3f2a20c05cd
dcdh <damien.clementdhuart@gmail.com>
2020-05-02 20:40:36 +0200

    fix tests. But why I had committed un-working tests ?

--- encryption/src/test/java/com/damdamdeo/eventdataspreader/eventsourcing/infrastructure/JacksonEncryptionDeserializerTest.java
+++ encryption/src/test/java/com/damdamdeo/eventdataspreader/eventsourcing/infrastructure/JacksonEncryptionDeserializerTest.java
@@ -1,18 +1,46 @@
 package com.damdamdeo.eventdataspreader.eventsourcing.infrastructure;
 
+import com.damdamdeo.eventdataspreader.eventsourcing.api.EncryptedEventSecret;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectReader;
 import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class JacksonEncryptionDeserializerTest {
 
+    private static final class TestEncryptedEventSecret implements EncryptedEventSecret {
+
+        private final String secret;
+
+        public TestEncryptedEventSecret(final String secret) {
+            this.secret = secret;
+        }
+
+        @Override
+        public String aggregateRootType() {
+            throw new IllegalStateException("should not be called");
+        }
+
+        @Override
+        public String aggregateRootId() {
+            throw new IllegalStateException("should not be called");
+        }
+
+        @Override
+        public String secret() {
+            return secret;
+        }
+    }
+
     @Test
     public void should_decrypt_name_when_key_is_present() throws Exception {
         // Given
-        final String secret = "wTObuvBniTpeYpfZfkafYqXKSugqGLWs";
-        final String serializedPerson = "{\"id\":\"id\",\"name\":\"48oOMAMakA0dgr/wfajOcQ==\"}";
+
+        final Optional<EncryptedEventSecret> secret = Optional.of(new TestEncryptedEventSecret("wTObuvBniTpeYpfZfkafYqXKSugqGLWs"));
+        final String serializedPerson = "{\"id\":\"id\",\"name\":\"3oihYNwwgO7qTN/Mg3ztwQ==\"}";
         final ObjectReader objectReader = new ObjectMapper().readerFor(Person.class)
                 .withAttribute(JacksonEncryptionSerializer.ENCODER_SECRET, secret);
 
@@ -28,7 +56,7 @@ public class JacksonEncryptionDeserializerTest {
         // Given
         final String serializedPerson = "{\"id\":\"id\",\"name\":\"48oOMAMakA0dgr/wfajOcQ==\"}";
         final ObjectReader objectReader = new ObjectMapper().readerFor(Person.class)
-                .withAttribute(JacksonEncryptionSerializer.ENCODER_SECRET, null);
+                .withAttribute(JacksonEncryptionSerializer.ENCODER_SECRET, Optional.empty());
 
         // When
         final Person person = objectReader.readValue(serializedPerson);
--- encryption/src/test/java/com/damdamdeo/eventdataspreader/eventsourcing/infrastructure/JacksonEncryptionSerializerTest.java
+++ encryption/src/test/java/com/damdamdeo/eventdataspreader/eventsourcing/infrastructure/JacksonEncryptionSerializerTest.java
@@ -1,19 +1,46 @@
 package com.damdamdeo.eventdataspreader.eventsourcing.infrastructure;
 
+import com.damdamdeo.eventdataspreader.eventsourcing.api.EncryptedEventSecret;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class JacksonEncryptionSerializerTest {
 
+    private static final class TestEncryptedEventSecret implements EncryptedEventSecret {
+
+        private final String secret;
+
+        public TestEncryptedEventSecret(final String secret) {
+            this.secret = secret;
+        }
+
+        @Override
+        public String aggregateRootType() {
+            throw new IllegalStateException("should not be called");
+        }
+
+        @Override
+        public String aggregateRootId() {
+            throw new IllegalStateException("should not be called");
+        }
+
+        @Override
+        public String secret() {
+            return secret;
+        }
+    }
+
     @Test
     public void should_encrypt_name_when_key_is_present() throws Exception {
         // Given
-        final String secret = "wTObuvBniTpeYpfZfkafYqXKSugqGLWs";
+        final Optional<EncryptedEventSecret> secret = Optional.of(new TestEncryptedEventSecret("wTObuvBniTpeYpfZfkafYqXKSugqGLWs"));
         final ObjectWriter objectWriter = new ObjectMapper().writer().withAttribute(JacksonEncryptionSerializer.ENCODER_SECRET, secret);
 
         final Person person = new Person("id", "Damien");
@@ -22,13 +49,14 @@ public class JacksonEncryptionSerializerTest {
         final String serializedPerson = objectWriter.writeValueAsString(person);
 
         // Then
-        assertEquals("{\"id\":\"id\",\"name\":\"48oOMAMakA0dgr/wfajOcQ==\"}", serializedPerson);
+        assertEquals("{\"id\":\"id\",\"name\":\"3oihYNwwgO7qTN/Mg3ztwQ==\"}", serializedPerson);
     }
 
     @Test
     public void should_fail_fast_when_key_is_not_present() throws Exception {
         // Given
-        final ObjectWriter objectWriter = new ObjectMapper().writer().withAttribute(JacksonEncryptionSerializer.ENCODER_SECRET, null);
+        final ObjectWriter objectWriter = new ObjectMapper().writer().withAttribute(JacksonEncryptionSerializer.ENCODER_SECRET,
+                Optional.empty());
 
         final Person person = new Person("id", "Damien");
 
