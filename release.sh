e81dee32d0ed3b75c4b17fe0237020d3f00593f7
dcdh <damien.clementdhuart@gmail.com>
2020-07-18 17:11:27 +0200

    avoid date in version to be able to dev from local to OpenShift using the same dependencies version

--- release.sh
+++ release.sh
@@ -4,7 +4,7 @@ then
   echo 'release cannot be performed when uncommited changes are presents'
   exit 1
 fi
-mvn versions:set -DnewVersion=1.0-$(date '+%Y%m%d-%H%M%S')-$(git rev-parse --short HEAD) -DprocessAllModules=true
+mvn versions:set -DnewVersion=1.0-$(git rev-parse --short HEAD) -DprocessAllModules=true
 docker kill $(docker ps -aq); docker rm $(docker ps -aq); docker volume prune -f; docker network prune -f;
 export TESTCONTAINERS_RYUK_DISABLED=true;
 mvn clean test install
