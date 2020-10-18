b23e16e2713d995b86878cec9e5c8c4a3c7763ab
dcdh <damien.clementdhuart@gmail.com>
2020-10-18 17:09:11 +0200

    Use hazelcast shared lock mechanism. Like this I can have multiple instance of mutable applications running in // and ensure that only one command will be executed at the same time.

--- eventsourced-mutable-infra/src/main/java/com/damdamdeo/eventsourced/mutable/infra/eventsourcing/command/CommandExecutor.java
+++ eventsourced-mutable-infra/src/main/java/com/damdamdeo/eventsourced/mutable/infra/eventsourcing/command/CommandExecutor.java
@@ -1,27 +1,35 @@
 package com.damdamdeo.eventsourced.mutable.infra.eventsourcing.command;
 
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.cp.lock.FencedLock;
+
 import javax.enterprise.context.ApplicationScoped;
+import java.util.Objects;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.locks.ReentrantLock;
 
 @ApplicationScoped
 public class CommandExecutor {
 
-    final ReentrantLock lock;
+    private static final String ONE_TIME_COMMAND_EXECUTION = "one-time-command-execution";
 
-    public CommandExecutor() {
-        this.lock = new ReentrantLock();
+    final HazelcastInstance hazelcastClient;
+
+    public CommandExecutor(final HazelcastInstance hazelcastClient) {
+        this.hazelcastClient = Objects.requireNonNull(hazelcastClient);
     }
 
     public <T> T execute(Callable<T> callable) throws Throwable {
+        final FencedLock fencedLock = hazelcastClient
+                .getCPSubsystem()
+                .getLock(ONE_TIME_COMMAND_EXECUTION);
         try {
-            lock.lock();
+            fencedLock.lock();
             return callable.call();
         } catch (final ExecutionException executionException) {
             throw executionException.getCause();
         } finally {
-            lock.unlock();
+            fencedLock.unlock();
         }
     }
 
--- eventsourced-mutable-infra/src/test/java/com/damdamdeo/eventsourced/mutable/infra/resources/InfraQuarkusTestResourceLifecycleManager.java
+++ eventsourced-mutable-infra/src/test/java/com/damdamdeo/eventsourced/mutable/infra/resources/InfraQuarkusTestResourceLifecycleManager.java
@@ -3,8 +3,10 @@ package com.damdamdeo.eventsourced.mutable.infra.resources;
 import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.Network;
 import org.testcontainers.containers.output.Slf4jLogConsumer;
+import org.testcontainers.containers.wait.strategy.Wait;
 
 import java.util.Collections;
 import java.util.Map;
@@ -16,6 +18,8 @@ public class InfraQuarkusTestResourceLifecycleManager implements QuarkusTestReso
     private Network network;
 
     private OkdPostgreSQLContainer<?> postgresMutableContainer;
+
+    private GenericContainer hazelcastContainer;
 
     @Override
     public Map<String, String> start() {
@@ -29,6 +33,21 @@ public class InfraQuarkusTestResourceLifecycleManager implements QuarkusTestReso
                 .withNetworkAliases("mutable");
         postgresMutableContainer.start();
         postgresMutableContainer.followOutput(logConsumer);
+        hazelcastContainer = new GenericContainer("hazelcast/hazelcast:4.0.3")
+                .withExposedPorts(5701)
+                .waitingFor(
+                        Wait.forLogMessage(".*is STARTED.*\\n", 1)
+                );
+        hazelcastContainer.start();
+        hazelcastContainer.followOutput(logConsumer);
+
+        System.setProperty("quarkus.hazelcast-client.cluster-name", "dev");
+        System.setProperty("quarkus.hazelcast-client.cluster-members", String.format("localhost:%d", hazelcastContainer.getMappedPort(5701)));
+        // System.setProperty("quarkus.hazelcast-client.outbound-port-definitions",);
+        // System.setProperty("quarkus.hazelcast-client.outbound-ports", );
+        // System.setProperty("quarkus.hazelcast-client.labels",);
+        // System.setProperty("quarkus.hazelcast-client.connection-timeout", "60");
+
         System.setProperty("quarkus.datasource.jdbc.url", postgresMutableContainer.getJdbcUrl());
         System.setProperty("quarkus.datasource.username", postgresMutableContainer.getUsername());
         System.setProperty("quarkus.datasource.password", postgresMutableContainer.getPassword());
@@ -46,11 +65,21 @@ public class InfraQuarkusTestResourceLifecycleManager implements QuarkusTestReso
         System.clearProperty("quarkus.datasource.mutable.jdbc.url");
         System.clearProperty("quarkus.datasource.mutable.username");
         System.clearProperty("quarkus.datasource.mutable.password");
+        System.clearProperty("quarkus.hazelcast-client.cluster-name");
+        System.clearProperty("quarkus.hazelcast-client.cluster-members");
+        System.clearProperty("quarkus.hazelcast-client.outbound-port-definitions");
+        System.clearProperty("quarkus.hazelcast-client.outbound-ports");
+        System.clearProperty("quarkus.hazelcast-client.labels");
+        System.clearProperty("quarkus.hazelcast-client.connection-timeout");
+
         if (postgresMutableContainer != null) {
             postgresMutableContainer.close();
         }
         if (network != null) {
             network.close();
+        }
+        if (hazelcastContainer != null) {
+            hazelcastContainer.close();
         }
     }
 
