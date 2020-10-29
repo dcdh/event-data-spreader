9eba5b0e0a990e6e767d28d1b6d1da9d6cf42f5d
dcdh <damien.clementdhuart@gmail.com>
2020-10-29 11:11:27 +0100

    Go Java 11 !

--- release.sh
+++ release.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# https://gist.github.com/ricardozanini/fa65e485251913e1467837b1c5a8ed28
+export GRAALVM_HOME=/usr/lib/jvm/graalvm/
+export JAVA_HOME=${GRAALVM_HOME}
+export PATH=${GRAALVM_HOME}/bin:$PATH
+
 if [[ -n $(git status -s) ]]
 then
   echo 'release cannot be performed when uncommited changes are presents'
--- run_tests_install.sh
+++ run_tests_install.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+# https://gist.github.com/ricardozanini/fa65e485251913e1467837b1c5a8ed28
+export GRAALVM_HOME=/usr/lib/jvm/graalvm/
+export JAVA_HOME=${GRAALVM_HOME}
+export PATH=${GRAALVM_HOME}/bin:$PATH
+
 docker kill $(docker ps -aq); docker rm $(docker ps -aq); docker volume prune -f; docker network prune -f; \
   mvn clean test install
