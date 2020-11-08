5c59282e00ef95016536cb5ce88038fb9afe29f3
dcdh <damien.clementdhuart@gmail.com>
2020-11-08 13:30:54 +0100

    Finished replacement of Kafka confluent image stack by Debezium stack.

--- INFRA.md
+++ INFRA.md
@@ -2,9 +2,10 @@
 
 ### docker
 
-docker pull confluentinc/cp-kafka:5.2.1 \
-    && docker pull debezium/postgres:11-alpine \
+docker pull debezium/postgres:11-alpine \
     && docker pull hazelcast/hazelcast:4.0.3 \
     && docker pull dcdh1983/postgresql-10-debezium-centos7:latest \
+    && docker pull debezium/kafka:1.3.0.Final \
+    && docker pull debezium/zookeeper:1.3.0.Final \
     && docker pull debezium/connect:1.3.0.Final \
     && docker pull registry.access.redhat.com/ubi8/ubi-minimal:8.1
--- eventsourced-mutable-publisher/src/test/java/com/damdamdeo/eventsourced/mutable/publisher/resources/DebeziumQuarkusTestResourceLifecycleManager.java
+++ eventsourced-mutable-publisher/src/test/java/com/damdamdeo/eventsourced/mutable/publisher/resources/DebeziumQuarkusTestResourceLifecycleManager.java
@@ -1,12 +1,12 @@
 package com.damdamdeo.eventsourced.mutable.publisher.resources;
 
-import io.debezium.testing.testcontainers.DebeziumContainer;
 import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.Network;
 import org.testcontainers.containers.output.Slf4jLogConsumer;
+import org.testcontainers.containers.wait.strategy.Wait;
 
 import java.util.Collections;
 import java.util.Map;
@@ -15,13 +15,19 @@ public class DebeziumQuarkusTestResourceLifecycleManager implements QuarkusTestR
 
     private final Logger logger = LoggerFactory.getLogger(DebeziumQuarkusTestResourceLifecycleManager.class);
 
+    private final static String DEBEZIUM_VERSION = "1.3.0.Final";
+    private final static Integer KAFKA_PORT = 9092;
+    private final static Integer DEBEZIUM_CONNECT_API_PORT = 8083;
+
     private Network network;
 
     private OkdPostgreSQLContainer<?> postgresMutableContainer;
 
-    private KafkaContainer kafkaContainer;
+    private GenericContainer<?> zookeeperContainer;
 
-    private DebeziumContainer debeziumContainer;
+    private GenericContainer<?> kafkaContainer;
+
+    private GenericContainer<?> debeziumConnectContainer;
 
     @Override
     public Map<String, String> start() {
@@ -38,20 +44,34 @@ public class DebeziumQuarkusTestResourceLifecycleManager implements QuarkusTestR
         System.setProperty("quarkus.datasource.mutable.jdbc.url", postgresMutableContainer.getJdbcUrl());
         System.setProperty("quarkus.datasource.mutable.username", postgresMutableContainer.getUsername());
         System.setProperty("quarkus.datasource.mutable.password", postgresMutableContainer.getPassword());
-        // confluentinc/cp-kafka:5.2.1
-        kafkaContainer = new KafkaContainer("5.2.1")
-                .withNetwork(network);
-        kafkaContainer.start();
-//        kafkaContainer.followOutput(logConsumer);
-        debeziumContainer = new DebeziumContainer("damdamdeo/eventsourced-mutable-kafka-connect:1.3.0.Final")
+
+        zookeeperContainer = new GenericContainer<>("debezium/zookeeper:" + DEBEZIUM_VERSION)
                 .withNetwork(network)
-                .withKafka(kafkaContainer)
-                .dependsOn(kafkaContainer);
-        debeziumContainer.start();
-        debeziumContainer.followOutput(logConsumer);
-        System.setProperty("mp.messaging.incoming.event-in.bootstrap.servers", kafkaContainer.getBootstrapServers());
+                .waitingFor(Wait.forLogMessage(".*Started.*", 1));
+        zookeeperContainer.start();
+        kafkaContainer = new GenericContainer<>("debezium/kafka:" + DEBEZIUM_VERSION)
+                .withNetwork(network)
+                .withExposedPorts(KAFKA_PORT)
+                .withEnv("ZOOKEEPER_CONNECT", String.format("%s:%d", zookeeperContainer.getNetworkAliases().get(0), 2181))
+                .withEnv("CREATE_TOPICS", "event:3:1:compact") // 3 partitions 1 replica
+                .waitingFor(Wait.forLogMessage(".*started.*", 1));
+        kafkaContainer.start();
+        kafkaContainer.followOutput(logConsumer);
+        debeziumConnectContainer = new GenericContainer<>("damdamdeo/eventsourced-mutable-kafka-connect:1.3.0.Final")
+                .withNetwork(network)
+                .withExposedPorts(DEBEZIUM_CONNECT_API_PORT)
+                .withEnv("BOOTSTRAP_SERVERS", String.format("%s:%d", kafkaContainer.getNetworkAliases().get(0), KAFKA_PORT))
+                .withEnv("GROUP_ID", "1")
+                .withEnv("CONFIG_STORAGE_TOPIC", "my_connect_configs")
+                .withEnv("OFFSET_STORAGE_TOPIC", "my_connect_offsets")
+                .withEnv("STATUS_STORAGE_TOPIC", "my_connect_statuses")
+                .waitingFor(Wait.forLogMessage(".*Finished starting connectors and tasks.*", 1));
+        debeziumConnectContainer.start();
+        debeziumConnectContainer.followOutput(logConsumer);
+        System.setProperty("mp.messaging.incoming.event-in.bootstrap.servers",
+                String.format("%s:%s", "localhost", kafkaContainer.getMappedPort(KAFKA_PORT)));
         System.setProperty("kafka-connector-api/mp-rest/url",
-                String.format("http://%s:%d", "localhost", debeziumContainer.getMappedPort(8083)));
+                String.format("http://%s:%d", "localhost", debeziumConnectContainer.getMappedPort(DEBEZIUM_CONNECT_API_PORT)));
         System.setProperty("connector.mutable.database.hostname", "mutable");
         System.setProperty("connector.mutable.database.username", postgresMutableContainer.getUsername());
         System.setProperty("connector.mutable.database.password", postgresMutableContainer.getPassword());
@@ -75,11 +95,14 @@ public class DebeziumQuarkusTestResourceLifecycleManager implements QuarkusTestR
             postgresMutableContainer.close();
         }
         System.clearProperty("mp.messaging.incoming.event-in.bootstrap.servers");
+        if (zookeeperContainer != null) {
+            zookeeperContainer.close();
+        }
         if (kafkaContainer != null) {
             kafkaContainer.close();
         }
-        if (debeziumContainer != null) {
-            debeziumContainer.close();
+        if (debeziumConnectContainer != null) {
+            debeziumConnectContainer.close();
         }
         if (network != null) {
             network.close();
