4f10a5eb06d9b7dd0b16ec18a90b901d0776dc8f
dcdh <damien.clementdhuart@gmail.com>
2021-02-11 19:30:15 +0100

    Extract an interface to be easily overloaded using @Alternative.

--- eventsourced-mutable-infra/src/main/java/com/damdamdeo/eventsourced/mutable/infra/eventsourcing/AggregateRootInstanceCreator.java
+++ eventsourced-mutable-infra/src/main/java/com/damdamdeo/eventsourced/mutable/infra/eventsourcing/AggregateRootInstanceCreator.java
@@ -2,19 +2,8 @@ package com.damdamdeo.eventsourced.mutable.infra.eventsourcing;
 
 import com.damdamdeo.eventsourced.mutable.api.eventsourcing.AggregateRoot;
 
-import javax.enterprise.context.ApplicationScoped;
+public interface AggregateRootInstanceCreator {
 
-@ApplicationScoped
-public class AggregateRootInstanceCreator {
-
-    public <T extends AggregateRoot> T createNewInstance(final Class<T> clazz,
-                                                         final String aggregateRootId) {
-        try {
-            return clazz.getDeclaredConstructor(String.class)
-                    .newInstance(aggregateRootId);
-        } catch (final Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
+    <T extends AggregateRoot> T createNewInstance(Class<T> clazz, String aggregateRootId);
 
 }
---
+++ eventsourced-mutable-infra/src/main/java/com/damdamdeo/eventsourced/mutable/infra/eventsourcing/ReflectionAggregateRootInstanceCreator.java
@@ -0,0 +1,21 @@
+package com.damdamdeo.eventsourced.mutable.infra.eventsourcing;
+
+import com.damdamdeo.eventsourced.mutable.api.eventsourcing.AggregateRoot;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class ReflectionAggregateRootInstanceCreator implements AggregateRootInstanceCreator {
+
+    @Override
+    public <T extends AggregateRoot> T createNewInstance(final Class<T> clazz,
+                                                         final String aggregateRootId) {
+        try {
+            return clazz.getDeclaredConstructor(String.class)
+                    .newInstance(aggregateRootId);
+        } catch (final Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
